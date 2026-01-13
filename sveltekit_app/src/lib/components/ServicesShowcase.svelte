fe26774f81ccd88f90ef398d6424145b36430530
Bloom Media Corporation <bloommediacorporation@gmail.com>
2026-01-13 18:29:27 +0200

    Fix Svelte 5 event handler syntax in ServicesShowcase

--- sveltekit_app/src/lib/components/ServicesShowcase.svelte
+++ sveltekit_app/src/lib/components/ServicesShowcase.svelte
@@ -75,10 +75,10 @@
                         class="col-start-1 row-start-1 w-full pointer-events-none"
                         in:fly={{ y: 50 * direction, duration: 900, easing: quartOut, opacity: 0 }}
                         out:fly={{ y: -50 * direction, duration: 900, easing: quartOut, opacity: 0 }}
-                        on:introstart={(e) => applyWillChange(e.currentTarget)}
-                        on:introend={(e) => clearWillChange(e.currentTarget)}
-                        on:outrostart={(e) => applyWillChange(e.currentTarget)}
-                        on:outroend={(e) => clearWillChange(e.currentTarget)}
+                        onintrostart={(e) => applyWillChange(e.currentTarget)}
+                        onintroend={(e) => clearWillChange(e.currentTarget)}
+                        onoutrostart={(e) => applyWillChange(e.currentTarget)}
+                        onoutroend={(e) => clearWillChange(e.currentTarget)}
                     >
                         <h2 class="text-white font-black text-[15vw] xl:text-[13vw] leading-[0.8] tracking-tighter uppercase mix-blend-difference">
                             {currentService.title}
@@ -119,10 +119,10 @@
                     in:fly={{ y: 20, duration: 600, delay: 200, easing: cubicOut }}
                     out:fade={{ duration: 200 }}
                     class="max-w-xl"
-                    on:introstart={(e) => applyWillChange(e.currentTarget)}
-                    on:introend={(e) => clearWillChange(e.currentTarget)}
-                    on:outrostart={(e) => applyWillChange(e.currentTarget)}
-                    on:outroend={(e) => clearWillChange(e.currentTarget)}
+                    onintrostart={(e) => applyWillChange(e.currentTarget)}
+                    onintroend={(e) => clearWillChange(e.currentTarget)}
+                    onoutrostart={(e) => applyWillChange(e.currentTarget)}
+                    onoutroend={(e) => clearWillChange(e.currentTarget)}
                 >
                     <p class="text-sm md:text-base leading-relaxed text-gray-300">
                         {currentService.description}
