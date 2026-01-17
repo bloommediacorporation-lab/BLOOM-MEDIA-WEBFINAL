035c364470d30a922b1afe74bf04ce8a7375815a
Bloom Media Corporation <bloommediacorporation@gmail.com>
2026-01-18 00:13:44 +0200

    final: cursor fix in Cookiebot banner – 100% done

--- sveltekit_app/src/cd.js
+++ sveltekit_app/src/cd.js
@@ -1,1 +1,15 @@
-<script id="CookieDeclaration" src="https://consent.cookiebot.com/c4162480-9dd0-4d9a-9d64-3527d2289375/cd.js" type="text/javascript" async></script>
+export function injectCookieDeclarationScript(target = document.head) {
+	if (typeof document === 'undefined') return;
+	if (document.getElementById('CookieDeclaration')) return;
+
+	const script = document.createElement('script');
+	script.id = 'CookieDeclaration';
+	script.src = 'https://consent.cookiebot.com/c4162480-9dd0-4d9a-9d64-3527d2289375/cd.js';
+	script.type = 'text/javascript';
+	script.async = true;
+
+	target.appendChild(script);
+}
+
+export const cookieDeclarationSrc =
+	'https://consent.cookiebot.com/c4162480-9dd0-4d9a-9d64-3527d2289375/cd.js';
